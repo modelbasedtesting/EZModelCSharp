3de631716d51edcb79030453ff866793d10b7375
Per Tempto <pertempto@harryrobinson.net>
2022-02-03 21:31:35 -0800

    VInding Machine initial commit

--- EzModelStateTable/ClientTemplate/ClientTemplate.cs
+++ EzModelStateTable/ClientTemplate/ClientTemplate.cs
@@ -7,13 +7,13 @@ using System.Collections.Generic;
 // Check the box for EzModelStateTable.
 using SeriousQualityEzModel;
 
-namespace ClientTemplate
+namespace VendingMachineNamespace
 {
-    class ClientTemplateProgram
+    class VendingMachineProgram
     {
         static int Main()
         {
-            TemplateClient client = new TemplateClient();
+            VendingMachine client = new VendingMachine();
             client.SelfLinkTreatment = SelfLinkTreatmentChoice.SkipAll;
             client.IncludeSelfLinkNoise = true;
 
@@ -64,7 +64,7 @@ namespace ClientTemplate
         }
     }
 
-    public class TemplateClient : IEzModelClient
+    public class VendingMachine : IEzModelClient
     {
         SelfLinkTreatmentChoice skipSelfLinks;
         bool notifyAdapter;
@@ -98,10 +98,19 @@ namespace ClientTemplate
             set => includeSelfLinkNoise = value;
         }
 
-        // TODO: Declare state variables
-        // Example:
-        bool svTrueFalse = true;
-        uint svSomeNumber = 4;
+        List<(string, object)> state = new List<(string, object)>
+        {
+            ("HasCoffeeIngredients", true),
+            ("HasTea", true),
+            ("HasHotWater", true),
+            ("InsufficientChange", false),
+            ("HasZeroMoney", true),
+            ("HasTeaMoney", false),
+            ("HasCoffeeMoney", false),
+            ("CoffeeSelected", false),
+            ("TeaSelected", false),
+            ("HotWaterSelected", false)
+        };
 
         // TODO: Declare transitions
         // SUGGESTION: Choose "const string" as the transition
@@ -109,27 +118,36 @@ namespace ClientTemplate
         // can be utilized in a switch case, which is useful in
         // GetEndState().
         // Example:
-        const string actionA = "Action A";
-        const string actionB = "Action B";
-        const string actionC = "Action C";
+        const string selectTea = "Select Tea";
+        const string selectCoffee = "Select Coffee";
+        const string selectHotWater = "Select Hot Water";
+        const string cancelSelection = "Cancel Selection";
+        const string refund = "Refund";
+        const string dispense = "Dispense";
 
-
-        string StringifyStateVector(bool stateArg1, uint stateArg2)
+        string StringifyStateVector(List<(string, object)> state)
         {
             // TODO: Join state variable values into an ordered string
 
             // Example:
-            string s = String.Format("StateArg1.{0}, StateArg2.{1}", stateArg1, stateArg2);
-            return s;
+
+            string stateString = "";
+            foreach((string s, object o) in state )
+            {
+                stateString += s + "." + o.ToString() + ", ";
+            }
+            if (stateString.Length > 2)
+            {
+                stateString = stateString.Substring(0, stateString.Length - 2);
+            }
+            Console.WriteLine(stateString);
+            return stateString;
         }
 
         // IEzModelClient Interface method
         public string GetInitialState()
         {
-            // TODO: feed state values
-
-            // Example:
-            return StringifyStateVector( svTrueFalse, svSomeNumber );
+            return StringifyStateVector( state );
         }
 
         // IEzModelClient Interface method
@@ -139,22 +157,45 @@ namespace ClientTemplate
 
             // TODO: Parse the startState.  Example:
             string[] vState = startState.Split(", ");
-            bool stateArg1 = vState[0].Contains("True") ? true : false;
-            uint stateArg2 = uint.Parse(vState[1].Split(".")[1]);
+
+            bool HasCoffeeIngredients = vState[0].Split(".")[1] == "True" ? true : false;
+            bool HasTea = vState[1].Split(".")[1] == "True" ? true : false;
+            bool HasHotWater = vState[2].Split(".")[1] == "True" ? true : false;
+            bool InsufficientChange = vState[3].Split(".")[1] == "True" ? true : false;
+            bool HasZeroMoney = vState[4].Split(".")[1] == "True" ? true : false;
+            bool HasTeaMoney = vState[5].Split(".")[1] == "True" ? true : false;
+            bool HasCoffeeMoney = vState[6].Split(".")[1] == "True" ? true : false;
+            bool CoffeeSelected = vState[7].Split(".")[1] == "True" ? true : false;
+            bool TeaSelected = vState[8].Split(".")[1] == "True" ? true : false;
+            bool HotWaterSelected = vState[9].Split(".")[1] == "True" ? true : false;
 
             // TODO: Accumulate actions available according to the start state.
             // Example:
-            if (!stateArg1)
+            if (HasCoffeeIngredients)
             {
-                actions.Add(actionA);
-                return actions;
+                actions.Add( selectCoffee );
             }
 
-            actions.Add(actionB);
-
-            if (includeSelfLinkNoise)
+            if (HasTea)
             {
-                actions.Add(actionC);
+                actions.Add( selectTea );
+            }
+
+            if (HasHotWater)
+            {
+                actions.Add( selectHotWater );
+            }
+
+            actions.Add( Cancel );
+
+            if (( CoffeeSelected && HasCoffeeMoney) || (TeaSelected && HasTeaMoney) || HotWaterSelected )
+            {
+                actions.Add( Dispense );
+            }
+
+            if (!HasZeroMoney)
+            {
+                actions.Add( Refund );
             }
 
             return actions;
@@ -167,8 +208,8 @@ namespace ClientTemplate
             // the action.
             // Example:
             string[] vState = startState.Split(", ");
-            bool stateArg1 = vState[0].Contains("True") ? true : false;
-            uint stateArg2 = uint.Parse(vState[1].Split(".")[1]);
+            bool stateArg1 = v(0].Contains("True") ? true : false;
+            uint stateArg2 = uint.Parse(v(1].Split(".")[1]);
 
             switch (action)
             {
